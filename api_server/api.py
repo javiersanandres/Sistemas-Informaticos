5b90a29a590d8ccd3be44c5efa632cb42dd2b17c
javier_sanandres <javier2017sanandres@gmail.com>
2024-11-12 15:31:41 +0100

    Exercise 2

---
+++ api_server/api.py
@@ -0,0 +1,169 @@
+import os
+import shutil
+import quart
+from dotenv import load_dotenv
+import sqlalchemy
+
+import utils
+
+app = quart.Quart(__name__)
+
+load_dotenv()
+
+
+@app.route('/file/<uid>', methods=['PUT'])
+async def create_user_library(uid):
+    """
+        Creates a library associated to a specific user. This action can only
+        be commanded by the users server. Any other attempt will be rejected.
+    """
+    auth_token = utils.get_access_token(
+        quart.request.headers.get('Authorization'))
+    if len(auth_token) == 0 or auth_token != os.getenv('SECRET'):
+        return quart.Response(
+            utils.build_unauthorized_response(), status=401)
+    else:
+        try:
+            os.mkdir(utils.build_absolute_path(f'file/{uid}'))
+            return quart.Response(status=201)
+        except OSError:
+            return quart.Response(
+                utils.build_internal_server_error(), status=500)
+
+
+@app.route('/file/<uid>', methods=['DELETE'])
+async def delete_user_library(uid):
+    """
+    Deletes the library associated to a specific user. That only happens
+    when the user is also pretended to be removed from the system and this
+    command can only come from users server.
+    """
+    auth_token = utils.get_access_token(
+        quart.request.headers.get('Authorization'))
+    if len(auth_token) == 0 or auth_token != os.getenv('SECRET'):
+        return quart.Response(
+            utils.build_unauthorized_response(), status=401)
+    else:
+        # Try to remove the user library from the system
+        if os.path.exists(utils.build_absolute_path(f'file/{uid}')):
+            shutil.rmtree(utils.build_absolute_path(f'file/{uid}'))
+            return quart.Response(status=200)
+        else:
+            return quart.Response(utils.build_not_found_response(), status=404)
+
+
+@app.route('/file/<uid>', methods=['GET'])
+async def list_documents(uid):
+    """
+    Lists the library associated to a user. It will return a 401 Unauthorized
+    Response if anybody but the user tries to list the library
+    """
+    auth_token = utils.get_access_token(
+        quart.request.headers.get('Authorization'))
+    if len(auth_token) == 0:
+        return quart.Response(
+            utils.build_unauthorized_response(), status=401)
+
+    # Validate token communicating with the user server
+    if utils.validate_token(auth_token, uid):
+        try:
+            files = os.listdir(utils.build_absolute_path(f'file/{uid}'))
+            return quart.Response(
+                response=utils.build_html_list_of_files(files))
+        except FileNotFoundError:
+            return quart.Response(utils.build_not_found_response(), status=404)
+    else:
+        return quart.Response(utils.build_unauthorized_response(), status=401)
+
+
+@app.route('/file/<uid>/<filename>', methods=['PUT'])
+async def add_file(uid, filename):
+    """
+    Adds a file to the user's library whose uid corresponds to the one in the
+    url and only allows the owner to do so. As it is stated in the practice
+    guide if the file already exists, then it is replaced with a new one.
+    """
+    auth_token = utils.get_access_token(
+        quart.request.headers.get('Authorization'))
+    if len(auth_token) == 0:
+        return quart.Response(
+            utils.build_unauthorized_response(), status=401)
+
+    data = await quart.request.get_data()
+
+    # Validate token communicating with the user server
+    if utils.validate_token(auth_token, uid):
+        file_path = utils.build_absolute_path(f'file/{uid}/{filename}')
+        update = os.path.isfile(file_path)
+        try:
+            with open(file_path, 'wb') as file:
+                file.write(data)
+
+            if update:
+                return quart.Response(f"File {filename} updated successfully "
+                                      "in user's library\n", status=201)
+            else:
+                return quart.Response(f"File {filename} added successfully in "
+                                      "user's library\n", status=201)
+
+        except OSError:
+            return quart.Response(
+                utils.build_internal_server_error(), status=500)
+    else:
+        return quart.Response(utils.build_unauthorized_response(), status=401)
+
+
+@app.route('/file/<uid>/<filename>', methods=['GET'])
+async def send_file(uid, filename):
+    """
+    This function sends a file from the user's library whose uid coincides
+    with the <uid> parameter and name coincides with the filename specified
+    by the requester.
+    """
+    auth_token = utils.get_access_token(
+        quart.request.headers.get('Authorization'))
+    if len(auth_token) == 0:
+        return quart.Response(
+            utils.build_unauthorized_response(), status=401)
+
+    # Validate token communicating with the user server
+    if utils.validate_token(auth_token, None):
+        file_path = utils.build_absolute_path(f'file/{uid}/{filename}')
+        if os.path.isfile(file_path):
+            return await quart.send_file(file_path, as_attachment=True)
+        else:
+            return quart.Response(utils.build_not_found_response(), status=404)
+    else:
+        return quart.Response(utils.build_unauthorized_response(), status=401)
+
+
+@app.route('/file/<uid>/<filename>', methods=['DELETE'])
+async def delete_file(uid, filename):
+    """
+    This function deletes a file from the user's library. Only the owner of
+    the library can remove files from it.
+    """
+    auth_token = utils.get_access_token(
+        quart.request.headers.get('Authorization'))
+    if len(auth_token) == 0:
+        return quart.Response(
+            utils.build_unauthorized_response(), status=401)
+
+    # Validate token communicating with the user server
+    if utils.validate_token(auth_token, uid):
+        file_path = utils.build_absolute_path(f'file/{uid}/{filename}')
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            return quart.Response(f"File {filename} successfully "
+                                  f"deleted from user's library\n", status=200)
+        else:
+            return quart.Response(utils.build_not_found_response(), status=404)
+    else:
+        return quart.Response(utils.build_unauthorized_response(), status=401)
+
+
+if __name__ == "__main__":
+    # Create file directory if it does not exist
+    os.makedirs(utils.build_absolute_path('file'), exist_ok=True)
+    app.run(host="file_app",
+            port=int(os.getenv('LIBRARY_SERVER_PORT')))
---
+++ api_server/utils.py
@@ -0,0 +1,161 @@
+import os
+import uuid
+
+
+def build_absolute_path(relative_filepath: str) -> str:
+    """
+    Build and absolute path for a given filepath.
+    """
+
+    absolute_path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(absolute_path, relative_filepath)
+
+    return filepath
+
+
+def build_bad_request_response(reason: str = "") -> str:
+    """
+    Builds an HTML response for a bad request.
+    """
+    response_html = "<!DOCTYPE html>\n" \
+                    "<html lang=\"en\">\n" \
+                    "<head>\n" \
+                    " <meta charset=\"UTF-8\">\n" \
+                    " <meta name=\"viewport\" content=\"width=device-width," \
+                    " initial-scale=1.0\">\n" \
+                    "    <title>400 Bad Request</title>\n" \
+                    "</head>\n" \
+                    "<body>\n" \
+                    "    <h1>400 Bad Request</h1>\n" \
+                    "    <p>   There is something wrong with the request " \
+                    "and could not be processed.   </p>\n"
+    if reason:
+        response_html += f"   <p>  {reason}   </p>\n"
+
+    response_html += "</body>\n" \
+                     "</html>\n"
+    return response_html
+
+
+def build_not_found_response() -> str:
+    """
+    Builds an HTML response for not found error.
+    """
+    response_html = "<!DOCTYPE html>\n" \
+                    "<html lang=\"en\">\n" \
+                    "<head>\n" \
+                    " <meta charset=\"UTF-8\">\n" \
+                    " <meta name=\"viewport\" content=\"width=device-width," \
+                    " initial-scale=1.0\">\n" \
+                    "    <title>404 Not Found</title>\n" \
+                    "</head>\n" \
+                    "<body>\n" \
+                    "    <h1>404 Not Found</h1>\n" \
+                    "    <p>The requested resource could not be" \
+                    " found on this server.</p>\n" \
+                    "</body>\n" \
+                    "</html>\n"
+
+    return response_html
+
+
+def build_unauthorized_response() -> str:
+    """
+    Builds an HTML response for unauthorized error.
+    """
+    response_html = "<!DOCTYPE html>\n" \
+                    "<html lang=\"en\">\n" \
+                    "<head>\n" \
+                    " <meta charset=\"UTF-8\">\n" \
+                    " <meta name=\"viewport\" content=\"width=device-width, " \
+                    " initial-scale=1.0\">\n" \
+                    "    <title>401 Unauthorized</title>\n" \
+                    "</head>\n" \
+                    "<body>\n" \
+                    "    <h1>401 Unauthorized</h1>\n" \
+                    " <p> You are not authorized to access this page. </p>\n" \
+                    "</body>\n" \
+                    "</html>\n"
+
+    return response_html
+
+
+def build_internal_server_error() -> str:
+    """
+    Builds an HTML response for internal server error.
+    """
+    response_html = "<!DOCTYPE html>\n" \
+                    "<html lang=\"en\">\n" \
+                    "<head>\n" \
+                    "  <meta charset=\"UTF-8\">\n" \
+                    "  <meta name=\"viewport\" content=\"width=device-width," \
+                    " initial-scale=1.0\">\n" \
+                    "    <title>500 Internal Server Error</title>\n" \
+                    "</head>\n" \
+                    "<body>\n" \
+                    "    <h1>500 Internal Server Error</h1>\n" \
+                    "  <p> Sorry, something went wrong in the server. </p>\n" \
+                    "</body>\n" \
+                    "</html>\n"
+
+    return response_html
+
+
+def build_html_list_of_files(files) -> str:
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>File List</title>
+    </head>
+    <body>
+            <h1>Files in Library</h1>
+    """
+
+    if not files:
+        html_content += "<p>There are no files in the library.</p>"
+    else:
+        html_content += "<ul>"
+        # Add each file to the HTML unordered list
+        for file in files:
+            html_content += f"<li>{file}</li>"
+        html_content += "</ul>"
+
+    html_content += """
+    </body>
+    </html>
+    """
+
+    return html_content
+
+
+def get_access_token(auth_header) -> str:
+    if auth_header is None:
+        return ""
+    auth_split = auth_header.split(" ")
+
+    if auth_split[0] == "Bearer" and len(auth_split) == 2:
+        return auth_split[1]
+    else:
+        return ""
+
+
+def validate_token(auth_header, uid) -> bool:
+    auth_header = auth_header.split('.')
+    if len(auth_header) != 2:
+        return False
+
+    if uid is None:
+        return uuid.UUID(
+            auth_header[1]) == uuid.uuid5(
+            uuid.UUID(
+                os.getenv('SECRET')),
+            auth_header[0])
+    else:
+        return uuid.UUID(
+            auth_header[1]) == uuid.uuid5(
+            uuid.UUID(
+                os.getenv('SECRET')),
+            uid)
