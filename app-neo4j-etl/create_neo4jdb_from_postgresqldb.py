775dc350acaf3b706061201a9a714a04fb72cdb8
ZCurro03 <currozv@gmail.com>
2024-11-22 18:17:29 +0100

    Neo4j database created

--- app-neo4j-etl/create_neo4jdb_from_postgresqldb.py
+++ app-neo4j-etl/create_neo4jdb_from_postgresqldb.py
@@ -1,12 +1,19 @@
 from typing import List, Tuple, Any
 import os
 from dotenv import load_dotenv
+import argparse
 import sqlalchemy as sql
-from neo4j import GraphDatabase, RoutingControl
+from sqlalchemy.orm import sessionmaker
+from neo4j import GraphDatabase
+from neo4j.exceptions import DriverError, Neo4jError
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+SQL_DATABASE_URI = os.getenv('SQL_DATABASE_URI')
+
+NEO4J_URI = os.getenv('NEO4J_URI')
+NEO4J_PSWRD = os.getenv('NEO4J_PSWRD')
+NEO4J_AUTH = ("neo4j", NEO4J_PSWRD)
 
 def get_data_from_sql_database(url: str) -> Tuple[List[Any]]:
     """
@@ -15,15 +22,15 @@ def get_data_from_sql_database(url: str) -> Tuple[List[Any]]:
     """
     
     # Connect to SQL database
-    engine = sql.create_engine(DATABASE_URL)
-    Session = sql.orm.sessionmaker(bind=engine)
+    engine = sql.create_engine(SQL_DATABASE_URI)
+    Session = sessionmaker(bind=engine)
     session = Session()
     
     # Fetch data from the database
     movies = get_best_selling_US_movies(session)
 
     # Get movie ids to reuse them to obtain the actors and directors
-    movie_ids = [movie['id'] for movie in movies]
+    movie_ids = [movie['movie_id'] for movie in movies]
 
     actors = get_movie_actors(session, movie_ids)
     directors = get_movie_directors(session, movie_ids)
@@ -40,7 +47,7 @@ def get_best_selling_US_movies(session: Any) -> List[Any]:
     """
     best_selling_US_movies_query = sql.text("""
                                     SELECT
-                                        m.movieid as movieId,
+                                        m.movieid as movie_id,
                                         m.movietitle AS title
                                     FROM
                                         imdb_movies m
@@ -58,8 +65,8 @@ def get_best_selling_US_movies(session: Any) -> List[Any]:
                                         SUM(i.sales) DESC
                                     LIMIT 20;
                                     """)
-    movies_result = session.execute(best_selling_US_movies_query)
-    movies = movies_result.fetchall()
+    movies_result = session.execute(best_selling_US_movies_query).mappings()
+    movies = movies_result.all()
     
     return movies
 
@@ -70,8 +77,8 @@ def get_movie_actors(session: Any, movie_ids: List) -> List[Any]:
     """
     actors_query = sql.text("""
                             SELECT
-                                m.movieid,
-                                a.actorid as actorId,
+                                m.movieid as movie_id,
+                                a.actorid as actor_id,
                                 a.actorname AS name
                             FROM
                                 imdb_movies m
@@ -82,8 +89,8 @@ def get_movie_actors(session: Any, movie_ids: List) -> List[Any]:
                             WHERE
                                 m.movieid IN :movie_ids;
                             """)
-    actors_result = session.execute(actors_query, {'movie_ids': tuple(movie_ids)})
-    actors = actors_result.fetchall()
+    actors_result = session.execute(actors_query, {'movie_ids': tuple(movie_ids)}).mappings()
+    actors = actors_result.all()
     
     return actors
 
@@ -94,9 +101,8 @@ def get_movie_directors(session: Any, movie_ids: List) -> List[Any]:
     """
     directors_query = sql.text("""
                             SELECT 
-                                m.movieid,
-                                m.movietitle AS movie_title,
-                                d.directorid AS directorId,
+                                m.movieid as movie_id,
+                                d.directorid AS director_id,
                                 d.directorname AS name
                             FROM 
                                 imdb_movies m
@@ -107,26 +113,103 @@ def get_movie_directors(session: Any, movie_ids: List) -> List[Any]:
                             WHERE 
                                 m.movieid IN :movie_ids
                             """)
-    directors_result = session.execute(directors_query, {'movie_ids': tuple(movie_ids)})
-    directors = directors_result.fetchall()
+    directors_result = session.execute(directors_query, {'movie_ids': tuple(movie_ids)}).mappings()
+    directors = directors_result.all()
     
     return directors
 
 
+def build_neo4j_db(data: Tuple[Any], clear_data: bool = False) -> None:
+    """
+    Builds the new Neo4j database with the previously fetched data.
+    """
+    # Create controller
+    driver = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
+
+    # Fill the database with info
+    try:
+        with driver.session() as session:
+            # Clear existing data, if requested
+            if clear_data:
+                print('[NEO4J-DB] Clearing existing data...')
+                session.run('MATCH (n) DETACH DELETE n')
+                print('[NEO4J-DB] Existing data cleared.')
+
+            # Add new data
+            add_movies(data[0], session)
+            add_actors(data[1], session)
+            add_directors(data[2], session)
+    except (DriverError, Neo4jError) as exception:
+        print(f'An error occurred: \n{exception}')
+        driver.close()
+        raise
+
+    # Close controller after all
+    driver.close()
+
+
+def add_movies(movies_data: List[Any], session: Any) -> None:
+    """
+    Adds the info about the movies to the neo4j database.
+    """
+    for movie in movies_data:
+        session.run("""
+                    CREATE (m: Movie{movieId: $id, title: $title})
+                    """,
+                    id=int(movie['movie_id']), title=movie['title']
+                )
+
+
+def add_actors(actors_data: List[Any], session: Any) -> None:
+    """
+    Adds the info about the actors to the neo4j database.
+    """
+    for actor in actors_data:
+        session.run("""
+                    MATCH (m: Movie{movieId: $movie_id})
+                    CREATE (a: Actor: Person {actorId: $actor_id, name: $name})
+                    CREATE (a)-[:ACTED_IN]->(m)
+                    """,
+                    movie_id=int(actor['movie_id']), actor_id=int(actor['actor_id']),
+                    name=actor['name']
+                )
+
+
+def add_directors(directors_data: List[Any], session: Any) -> None:
+    """
+    Adds the info about the directors to the neo4j database.
+    """
+    for director in directors_data:
+        session.run("""
+                    MATCH (m: Movie{movieId: $movie_id})
+                    CREATE (d: Director: Person {directorId: $director_id, name: $name})
+                    CREATE (d)-[:DIRECTED]->(m)
+                    """,
+                    movie_id=int(director['movie_id']), director_id=int(director['director_id']),
+                    name=director['name']
+                )
+
+
+def parse_arguments():
+    """
+    Parse command-line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Manage the Neo4j database")
+    parser.add_argument("--clear_data", "-c", action="store_true",
+        help="Clear existing data in the Neo4j database.")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    movies, actors, directors = get_data_from_sql_database(DATABASE_URL)
+    # Check if "clear_data" option was selected
+    args = parse_arguments()
 
-    # Mostrar las 20 películas más vendidas
-    print("Top 20 Best-Selling Movies:")
-    for movie in movies:
-        print(f"Movie ID: {movie['movieid']}, Title: {movie['movie_title']}, Year: {movie['year']}, Total Sales: {movie['total_sales']}")
-
-    # Mostrar los actores de las 20 películas más vendidas
-    print("\nActors in the Top 20 Best-Selling Movies:")
-    for actor in actors:
-        print(f"Movie Title: {actor['movie_title']}, Actor: {actor['actor_name']}")
-
-    # Mostrar los directores de las 20 películas más vendidas
-    print("\nDirectors of the Top 20 Best-Selling Movies:")
-    for director in directors:
-        print(f"Movie Title: {director['movie_title']}, Director: {director['director_name']}")
+    # Fetch data from SQL database
+    print('[SQL-DB] Fetching data from SQL database...')
+    movies, actors, directors = get_data_from_sql_database(SQL_DATABASE_URI)
+    print('[SQL-DB] Data fetched correctly.')
+    
+    # Build the new Neo4j database
+    print('[NEO4J-DB] Building neo4j database from the fetched data...')
+    build_neo4j_db((movies, actors, directors), clear_data=args.clear_data)
+    print('[NEO4J-DB] Database was built correctly. Visit \"localhost:7474\" to check the database.')
