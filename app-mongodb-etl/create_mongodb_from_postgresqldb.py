e64e745c1d6d7158a0232b0771e8367b1aeb0fe1
javier_sanandres <javier2017sanandres@gmail.com>
2024-12-12 18:51:27 +0100

    Little bit of refactoring

--- app-mongodb-etl/create_mongodb_from_postgresqldb.py
+++ app-mongodb-etl/create_mongodb_from_postgresqldb.py
@@ -26,18 +26,23 @@ client = MongoClient(MONGODB_URL)
 mongo_db = client[MONGO_DB_NAME]
 mongo_collection = mongo_db[MONGO_COLLECTION_NAME]
 
+
 def get_french_movies(conn):
     """
     Gets all French movies or all in which France has collaborated
     """
     query = (
-        select(movies_table.c.movieid, movies_table.c.movietitle, movies_table.c.year)
-        .join(countries_table, movies_table.c.movieid == countries_table.c.movieid)
-        .where(countries_table.c.country == 'France')
-    )
+        select(
+            movies_table.c.movieid,
+            movies_table.c.movietitle,
+            movies_table.c.year) .join(
+            countries_table,
+            movies_table.c.movieid == countries_table.c.movieid) .where(
+                countries_table.c.country == 'France'))
     return conn.execute(query).fetchall()
 
-def get_related_movies(conn, movie_id, genres, most_related = True):
+
+def get_related_movies(conn, movie_id, genres, most_related=True):
     """
     Gets related movies based on genres coincidences
     """
@@ -49,34 +54,40 @@ def get_related_movies(conn, movie_id, genres, most_related = True):
             movies_table.c.year
         )
         .select_from(
-            genres_table.join(movies_table, movies_table.c.movieid == genres_table.c.movieid)
+            genres_table.join(movies_table,
+                              movies_table.c.movieid == genres_table.c.movieid)
         )
         .join(countries_table, movies_table.c.movieid == countries_table.c.movieid)
         .where(
             and_(
                 genres_table.c.genre.in_(genres),  # Filter genres
-                countries_table.c.country == 'France', # Only the French related Films
+                countries_table.c.country == 'France',  # Only the French related Films
                 genres_table.c.movieid != movie_id  # Exclude the current movie
             )
         )
-        .group_by(movies_table.c.movieid, movies_table.c.movietitle, movies_table.c.year)  # Group by movie details
+        # Group by movie details
+        .group_by(movies_table.c.movieid, movies_table.c.movietitle, movies_table.c.year)
         .order_by(movies_table.c.year.desc())  # Order by most recent movies
     )
 
     if most_related:
-        having_clause = func.count(genres_table.c.genre) == len(genres)  # Exact genre match
+        having_clause = func.count(genres_table.c.genre) == len(
+            genres)  # Exact genre match
     else:
         having_clause = and_(
-            func.count(genres_table.c.genre) >= len(genres) / 2,  # Minimum threshold
-            func.count(genres_table.c.genre) < len(genres)        # Exclude full matches
+            func.count(genres_table.c.genre) >= len(
+                genres) / 2,  # Minimum threshold
+            func.count(genres_table.c.genre) < len(
+                genres)        # Exclude full matches
         )
 
     # Complete query
     query = base_query.having(having_clause)
 
-    return [{"title" : related_movie[0].split(" (")[0],
-             "year" : int(related_movie[1])}
+    return [{"title": related_movie[0].split(" (")[0],
+             "year": int(related_movie[1])}
             for related_movie in conn.execute(query).fetchall()[:10]]
+
 
 def create_mongo_documents():
     """
@@ -96,29 +107,36 @@ def create_mongo_documents():
                 select(genres_table.c.genre)
                 .where(genres_table.c.movieid == movie_id)
             )
-            genres = [genre[0] for genre in connection.execute(genres_query).fetchall()]
+            genres = [genre[0]
+                      for genre in connection.execute(genres_query).fetchall()]
 
             # Get all directors of the movie
             directors_query = (
-                select(directors_table.c.directorname)
-                .join(directors_info_table, directors_table.c.directorid == directors_info_table.c.directorid)
-                .where(directors_info_table.c.movieid == movie_id)
-            )
-            directors = [director[0] for director in connection.execute(directors_query).fetchall()]
+                select(
+                    directors_table.c.directorname) .join(
+                    directors_info_table,
+                    directors_table.c.directorid == directors_info_table.c.directorid) .where(
+                    directors_info_table.c.movieid == movie_id))
+            directors = [director[0] for director in connection.execute(
+                directors_query).fetchall()]
 
             # Get all actors of the movie
             actors_query = (
-                select(actors_table.c.actorname)
-                .join(actors_info_table, actors_table.c.actorid == actors_info_table.c.actorid)
-                .where(actors_info_table.c.movieid == movie_id)
-            )
-            actors = [actors[0] for actors in connection.execute(actors_query).fetchall()]
+                select(
+                    actors_table.c.actorname) .join(
+                    actors_info_table,
+                    actors_table.c.actorid == actors_info_table.c.actorid) .where(
+                    actors_info_table.c.movieid == movie_id))
+            actors = [actors[0]
+                      for actors in connection.execute(actors_query).fetchall()]
 
             # Get most related movies based on genre coincidence
-            most_related = get_related_movies(connection, movie_id, genres, most_related=True)
+            most_related = get_related_movies(
+                connection, movie_id, genres, most_related=True)
 
             # Get related movies based on genre coincidence
-            related = get_related_movies(connection, movie_id, genres, most_related=False)
+            related = get_related_movies(
+                connection, movie_id, genres, most_related=False)
 
             # Create document for mongoDB database
             document = {
@@ -133,8 +151,7 @@ def create_mongo_documents():
 
             mongo_collection.insert_one(document)
 
+
 if __name__ == "__main__":
     create_mongo_documents()
     print("All data inserted in MongoDB successfully.")
-
-
--- app-mongodb-etl/mongodb_queries.py
+++ app-mongodb-etl/mongodb_queries.py
@@ -1,29 +1,38 @@
 from pymongo import MongoClient
 
+
 def first_query(db):
     print("SHOWING ALL FRENCH SCIENCE FICTION MOVIES BETWEEN 1994 AND 1998")
-    query = {"year": {"$gte": 1994, "$lte": 1998}, "genres": {"$in": ["Sci-Fi"]}}
+    query = {
+        "year": {
+            "$gte": 1994, "$lte": 1998}, "genres": {
+            "$in": ["Sci-Fi"]}}
     results = db.find(query, {"_id": 0})
     for result in results:
         print(result)
     print()
+
 
 def second_query(db):
     print("SHOWING ALL FRENCH 1998 DRAMAS WITH TITLE STARTING WITH \'THE\'")
-    query = {"year": { "$eq": 1998 }, "title" : { "$regex": ", The"}, "genres": { "$in": ["Drama"] }}
+    query = {
+        "year": {
+            "$eq": 1998}, "title": {
+            "$regex": ", The"}, "genres": {
+                "$in": ["Drama"]}}
     results = db.find(query, {"_id": 0})
     for result in results:
         print(result)
     print()
+
 
 def third_query(db):
     print("SHOWING ALL FRENCH FILMS IN WHICH AYE DUNAWAY y VIGGO MORTENSEN HAVE BOTH PLAYED")
-    query = { "actors": { "$all": ["Dunaway, Faye", "Mortensen, Viggo"] } }
+    query = {"actors": {"$all": ["Dunaway, Faye", "Mortensen, Viggo"]}}
     results = db.find(query, {"_id": 0})
     for result in results:
         print(result)
     print()
-
 
 
 if __name__ == "__main__":
@@ -37,4 +46,3 @@ if __name__ == "__main__":
     first_query(mongo_collection)
     second_query(mongo_collection)
     third_query(mongo_collection)
-
